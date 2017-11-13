798f1db0244386d16e3edc540cca1cc71525424f
Alex Biederer <alexander@biederer.de>
2017-11-13 22:04:26 +0100

    Jetzt auch mit dem richtigen file

--- Aufgabenblatt_4/generator/app/sql/sitzeProBundesland_2.sql
+++ Aufgabenblatt_4/generator/app/sql/sitzeProBundesland_2.sql
@@ -1,12 +1,11 @@
 with recursive mandatProWahlkreis(wahlkreis, partei) as
 (
-	select wahlkreis, partei 
-	from aggErst17
-	group by wahlkreis
+	select agg1.wahlkreis, agg1.partei
+	from aggErst17 agg1 LEFT JOIN aggErst17 agg2
+	on (agg1.wahlkreis = agg2.Wahlkreis and agg1.numStimmen < agg2.numStimmen)
+	where agg2.numStimmen IS NULL
 
-)
-
-select * from mandatProWahlkreis;
+),
 
 mandateProBundesland(bundesland, partei, numMandate) as
 (
@@ -37,7 +36,7 @@ sl(divisor, bundesland, wert) as (
 	)
 ),
 
-sitzeProBL (bundesland, sitze) as 
+sitzeProBL (bundesland, numSitze) as 
 ( 
 	select bundesland, count(*) 
 	from (
@@ -66,9 +65,9 @@ zweitProBLProParteiVor(bundesland, partei, numStimmen) as
 
 zweitProBLProPartei(bundesland, partei, numStimmen) as
 (
-	select zpbp.bundesland, zpbp.partei, zpbp.numStimmen 
-	from zweitProBLProParteiVor zpbp, zweitProBLVor zpb, aggErst17 agg, mandateProPartei mpp
-	where (zpbp.numStimmen*1.0)/(zpb.numStimmen) > 0.05
+	select distinct zpbp.bundesland, zpbp.partei, zpbp.numStimmen 
+	from zweitProBLProParteiVor zpbp, zweitProBLVor zpb, mandateProPartei mpp
+	where ((zpbp.numStimmen)/(zpb.numStimmen)) > 0.05
 	or mpp.numMandate >= 3
 ),
 
@@ -77,6 +76,13 @@ zweitProBL(bundesland, numStimmen) as
 	select bundesland, count(*)
 	from zweitProBLProPartei
 	group by bundesland
+),
+
+zweitProPartei(partei, numStimmen) as
+(
+	select partei, sum(numStimmen)
+    from zweitProBLProPartei
+    group by partei
 ),
 
 sl2(divisor, partei, bundesland, wert) as (
@@ -89,13 +95,13 @@ sl2(divisor, partei, bundesland, wert) as (
 	(
 	select sl.divisor + 1, sl.partei, sl.bundesland, (zpb.numStimmen*1.00)/(sl.divisor+1.0) as wert
 	from sl2 sl, zweitProBLProPartei zpb
-	where divisor < (select sitze from sitzeProBL where sl.bundesland = bundesland)
+	where divisor < (select numSitze from sitzeProBL where sl.bundesland = bundesland)
 	and zpb.bundesland = sl.bundesland
 	and zpb.partei = sl.partei
 	)
 ),
 
-sitzeProParteiProBL(bundesland, partei, sitze) as
+sitzeProParteiProBL(bundesland, partei, numSitze) as
 (
 	select b.id, p.id, 
 	(
@@ -104,11 +110,105 @@ sitzeProParteiProBL(bundesland, partei, sitze) as
 			select * from sl2 sl
 			where b.id = sl.bundesland
 			order by wert desc
-			limit (select sitze from sitzeProBL spb where spb.bundesland = b.id)
+			limit (select numSitze from sitzeProBL spb where spb.bundesland = b.id)
+		) as sllimit
+		where partei = p.id
+	)
+	from bundesland b, partei17 p
+),
+
+sitzeProPartei(partei, numSitze) as
+(
+	select partei, sum(numSitze)
+    from sitzeProParteiProBL
+    group by partei
+),
+
+minSitzeProPartei(partei, numSitze) as
+(
+	select spp.partei, (case when spp.numSitze > mpp.nummandate then spp.numSitze else mpp.nummandate end)
+    from sitzeProPartei spp, mandateProPartei mpp
+    where spp.partei = mpp.partei
+),
+
+sl3(divisor, partei, wert) as (
+	(
+	select 0.5 as divisor, mpp.partei, zpp.numStimmen/0.5 as wert
+	from minSitzeProPartei mpp, zweitProPartei zpp
+	where zpp.partei = mpp.partei
+	)
+	union
+	(
+	select sl.divisor + 1, sl.partei, (zpp.numStimmen)/(sl.divisor+1) as wert
+	from sl3 sl, zweitProPartei zpp
+	where divisor < (select numSitze from minSitzeProPartei mspp where sl.partei = mspp.partei)
+	and zpp.partei = sl.partei
+	)
+),
+
+sitzeProParteiUeberhang(partei, numSitze) as
+(
+	select partei, count(*)
+    from sl3
+    group by partei
+),
+
+sl4(divisor, bundesland, wert) as (
+	(
+	select 0.5 as divisor, id, numEinwohner/0.5 as wert
+	from bundesland
+	)
+	union
+	(
+	select divisor + 1, id, numEinwohner/(divisor+1) as wert
+	from sl, bundesland
+	where divisor < (select sum(numSitze) from sitzeProParteiUeberhang)
+	and id = bundesland
+	)
+),
+
+sitzeProBLAusgleich (bundesland, numSitze) as 
+( 
+	select bundesland, count(*) 
+	from (
+		select *
+		from sl4
+		order by wert DESC
+		limit (select sum(numSitze) from sitzeProParteiUeberhang)) as tmp
+	group by bundesland
+),
+
+sl5(divisor, partei, bundesland, wert) as (
+	(
+	select 0.5 as divisor, zpb.partei, zpb.bundesland, zpb.numStimmen/0.5 as wert
+	from zweitProBLProPartei zpb
+	)
+	union
+	(
+	select sl.divisor + 1, sl.partei, sl.bundesland, (zpb.numStimmen)/(sl.divisor+1.0) as wert
+	from sl2 sl, zweitProBLProPartei zpb
+	where divisor < (select numSitze from sitzeProBLAusgleich spba where sl.bundesland = spba.bundesland)
+	and zpb.bundesland = sl.bundesland
+	and zpb.partei = sl.partei
+	)
+),
+
+sitzeProParteiProBLAusgleich(bundesland, partei, numSitze) as
+(
+	select b.id, p.id, 
+	(
+		select count(*) from
+		(
+			select * from sl5 sl
+			where b.id = sl.bundesland
+			order by wert desc
+			limit (select numSitze from sitzeProBLAusgleich spba where spba.bundesland = b.id)
 		) as sllimit
 		where partei = p.id
 	)
 	from bundesland b, partei17 p
 )
 
-select sum(sitze) from sitzeProParteiProBL;
+select sum(numsitze) from sitzeProParteiProBLAusgleich ;
+
+
