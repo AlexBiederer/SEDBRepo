26307d13a7521cc61cbf61865367f6f69459975d
Alex Biederer <alexander@biederer.de>
2017-12-09 16:57:38 +0100

    added in deaggregate sql

---
+++ generator/backend/sql/zweit13.sql
@@ -0,0 +1,12 @@
+drop table zweit13;
+
+create table zweit13(partei, wahlkreis) as (
+
+	with recursive zweit13withNum(partei, wahlkreis, numStimmen) as 
+	((select * from aggzweit13
+	where numstimmen > 0)
+	union all
+	(select partei, wahlkreis, numstimmen-1 from zweit13withNum where numstimmen > 1))
+	(select partei, wahlkreis from erst17withNum)
+
+)
