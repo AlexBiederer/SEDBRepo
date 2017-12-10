d7760aa52fe4f9a2b6b4e86f09b9541ac3f08594
Felix Schwarzmeier <felixsla21@gmail.com>
2017-12-10 01:37:52 +0100

    Code cleanup + bug fixing

--- generator/backend/sql/zweit13.sql
+++ generator/backend/sql/zweit13.sql
@@ -2,11 +2,11 @@ drop table zweit13;
 
 create table zweit13(partei, wahlkreis) as (
 
-	with recursive zweit13withNum(partei, wahlkreis, numStimmen) as 
+	with recursive zweit13withNum(partei, wahlkreis, numStimmen) as
 	((select * from aggzweit13
 	where numstimmen > 0)
 	union all
 	(select partei, wahlkreis, numstimmen-1 from zweit13withNum where numstimmen > 1))
-	(select partei, wahlkreis from erst17withNum)
+	(select partei, wahlkreis from zweit13withNum)
 
 )
--- generator/backend/sql/zweit17.sql
+++ generator/backend/sql/zweit17.sql
@@ -2,11 +2,11 @@ drop table zweit17;
 
 create table zweit17(partei, wahlkreis) as (
 
-	with recursive zweit17withNum(partei, wahlkreis, numStimmen) as 
+	with recursive zweit17withNum(partei, wahlkreis, numStimmen) as
 	((select * from aggzweit17
 	where numstimmen > 0)
 	union all
 	(select partei, wahlkreis, numstimmen-1 from zweit17withNum where numstimmen > 1))
-	(select partei, wahlkreis from erst17withNum)
+	(select partei, wahlkreis from zweit17withNum)
 
 )
