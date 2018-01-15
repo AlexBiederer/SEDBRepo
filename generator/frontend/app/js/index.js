cd7b68516bcccba829828fc6c905d97090eeab3c
Felix Schwarzmeier <felixsla21@gmail.com>
2018-01-15 21:49:19 +0100

    Add routes

--- generator/frontend/app/js/index.js
+++ generator/frontend/app/js/index.js
@@ -3,25 +3,52 @@ import Bundestag from './bundestag';
 import Bundesland from './bundesland';
 import Wahlkreis from './wahlkreis';
 import StimmenAbgabe from './stimmenAbgabe';
+import Navigo from 'Navigo';
 
 $(function() {
-  new Bundestag();
-  new Bundesland();
-  new StimmenAbgabe();
+  const root = null;
+  const useHash = true;
+  const router = new Navigo(root, useHash);
+  const stimmenAbgabe = new StimmenAbgabe();
+  const bundesland = new Bundesland(router);
+  const bundestag =  new Bundestag();
+  // Main route
+  router
+    .on(_ => {
+      router.navigate(`/bundestag`);
+    })
+    .resolve();
+  // Other routes
+  router
+    .on({
+      '/stimmenAbgabe/:wk/:key': params => {
+        $(`.navbar-nav a[href="#stimmenAbgabe"]`).tab('show');
+        stimmenAbgabe.update(params)
+      },
+      '/wahlkreis': _ => $(`.navbar-nav a[href="#wahlkreis"]`).tab('show'),
+      '/wahlkreis/:wkID': params => {
+        bundesland.updateBarChart(params.wkID)
+        $(`.navbar-nav a[href="#wahlkreis"]`).tab('show');
+      },
+      '/bundesland': _ => $(`.navbar-nav a[href="#bundesland"]`).tab('show'),
+      '/bundestag': _ => $(`.navbar-nav a[href="#bundestag"]`).tab('show'),
+      '/stimmenAbgabe': _ => {
+        $(`.navbar-nav a[href="#stimmenAbgabe"]`).tab('show');
+        router.navigate(`/stimmenAbgabe/228/1234`);
+      }
+    })
+    .resolve();
+
   // Collapse navbar on click
   $('.nav a').on('click', function() {
     $('.navbar-toggle').click();
   });
-  // Hide Wahlkreis tab at first
+  // Hide Wahlkreis tab at first & activate routing on tab-click
   $('a[data-toggle="tab"]').on('click', function() {
+    const target = $(this).attr("href").split('#')[1].trim();
     if ($(this).parent('li').hasClass('disabled')) {
       alert("Bitte wählen Sie zuerst einen Wahlkreis im Reiter 'Bundesland' aus!");
       return false;
-    };
-  });
-  // Open Sozialversicherungsnummer Modal on tab-show
-  $('.nav a').on('shown.bs.tab', function(event) {
-    if($(event.target).text() === "Stimmenabgabe") $("#sozNrModal").modal("show");
-
+    } else router.navigate(`/${target}`);
   });
 });
