427869fbd0dcf1bcfb885f6ca32783813f207ed6
Felix Schwarzmeier <felixsla21@gmail.com>
2018-01-17 20:19:32 +0100

    Add Überhangmandate and sitze table to bundestag

--- generator/frontend/app/js/bundestag.js
+++ generator/frontend/app/js/bundestag.js
@@ -1,12 +1,13 @@
 import PieChart from './pieChart';
 import initDataTable from './dataTable';
+import IDtoBundesland from './IDtoBundesland';
 
 class Bundestag {
   constructor() {
     // create pie chart
-    new PieChart('#pieRoot', 'db/bundesland', {
+    new PieChart('#pieRoot', {
       value: 'sitze',
-      durationInit: 0,
+      durationInit: 200,
       durationMouse: 100
     });
     this.initSwitch();
@@ -25,65 +26,60 @@ class Bundestag {
         .draw();
       checked = $("#cb1:checked").length;
       if (checked) {
-        console.log("Checked");
         $.getJSON("db/query/bundestagsmitglieder17_update", _ => {
           this.dataTable.clear().draw();
           this.fillTable();
         });
       } else {
-        console.log("not Checked");
         this.dataTable.clear().draw();
         this.fillTable();
       }
     }.bind(this));
   }
 
-  // fill table Mitglieder
+  // init tables
   fillTableInit() {
-    console.log("fillTable");
     $.getJSON("db/mview/bundestagsmitglieder17", data => {
-      $("#membersTable").append(`
-      <thead>
-      <tr>
-        <th>Titel</th>
-        <th>Vorname</th>
-        <th>Nachname</th>
-        <th>Geschlecht</th>
-        <th>Gebjahr</th>
-        <th>Partei</th>
-      </tr>
-      </thead>
-      <tbody id="membersTableBody">
-      </tbody>
-    `);
-      data.forEach(val => {
-        $("#membersTableBody").append(
-          `
-        <tr>
-        <td>${val.titel || ''}</td>
-        <td>${val.vorname}</td>
-        <td>${val.name}</td>
-        <td>${val.geschlecht}</td>
-        <td>${val.gebjahr}</td>
-        <td>${val.pname}</td>
-        </tr>
-      `
-        );
-      });
-
-      // init data table
-      if (!this.dataTable)
-        this.dataTable = initDataTable("membersTable", {
-          "order": [
-            [2, "asc"]
-          ]
+      $.getJSON("db/query/q5", data2 => {
+        // fill Mitglieder table
+        this.fillTable();
+        // fill Überhangmandate Tables
+        let tmparray = [];
+        data2.forEach(val => {
+          if (val.nummandate > 0) { // only show parties/states with überhangmandaten
+            tmparray.push($(
+              `
+          <tr>
+          <td>${val.parteiname}</td>
+          <td>${IDtoBundesland[val.bundesland]}</td>
+          <td>${val.nummandate}</td>
+          </tr>
+        `
+            )[0]);
+          }
         });
+        // init data table mitglieder
+        if (!this.dataTable) {
+          this.dataTable = initDataTable("membersTable", {
+            "order": [
+              [2, "asc"]
+            ]
+          });
+        }
+        // init data table überhangmandate
+        if (!this.dataTableUeberhang) {
+          this.dataTableUeberhang = initDataTable("ueberhangTable", {});
+          this.dataTableUeberhang.rows.add(tmparray).draw();
+          this.dataTableUeberhang.columns.adjust().draw()
+        }
+      });
     });
   }
 
   // fill table Mitglieder
   fillTable() {
     $.getJSON("db/mview/bundestagsmitglieder17", data => {
+
       let tmparray = [];
       data.forEach(val => {
         tmparray.push($(
@@ -98,7 +94,6 @@ class Bundestag {
         </tr>
       `
         )[0]);
-
       });
       this.dataTable.rows.add(tmparray).draw();
       this.dataTable.columns.adjust().draw()
