5bbd8ec4fabc76b8cde37f0f560b6b570cb164df
Felix Schwarzmeier <felixsla21@gmail.com>
2018-01-17 20:19:50 +0100

    Add beruf to erstkandidaten query

--- generator/backend/js/query/erstKandidaten.js
+++ generator/backend/js/query/erstKandidaten.js
@@ -1,6 +1,6 @@
 module.exports = (wahlkreis) =>
 `
-select w.name as wkName, k.id, k.titel, k.vorname, k.name, p.name as partei, p.id as pid from kandidat17 k, partei17 p, direkt17 d, wahlkreis17 w
+select w.name as wkName, k.id, k.titel, k.vorname, k.name, k.beruf, p.name as partei, p.id as pid from kandidat17 k, partei17 p, direkt17 d, wahlkreis17 w
 where k.partei = p.id
 and d.kandidat = k.id
 and d.wahlkreis = ${wahlkreis}
